cb728bf1c55019131c03d9294d2c53e05ed13bfb
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2024-12-28 15:32:20 +0400

    "Introduced state to Educational Experience, and ability to add more fields"

--- src/components/edit-bar.jsx
+++ src/components/edit-bar.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+
 export default function EditBar() {
+  const [eduParts, setEduParts] = useState([1]);
+
+  function handleAddEduParts() {
+    if (eduParts.length < 4) {
+      setEduParts((prev) => [...prev, prev.length + 1]);
+    } else {
+      alert("Too many fields for education");
+    }
+  }
+
+  function handleRemoveEduParts() {
+    if (eduParts.length > 1) {
+      setEduParts((prev) => prev.slice(0, -1));
+    } else {
+      alert("Having atleast one educational field is mandatory");
+    }
+  }
+
   return (
     <>
       <GeneralInformation></GeneralInformation>
-      <EducationalExperience></EducationalExperience>
+      <div id="education-container">
+        {eduParts.map((eduPart) => (
+          <EducationalExperience index={eduPart}></EducationalExperience>
+        ))}
+        <button onClick={handleAddEduParts}>Add More</button>
+        <button onClick={handleRemoveEduParts}>Remove</button>
+      </div>
       <PracticalExperience></PracticalExperience>
       <MoreInformation></MoreInformation>
     </>
@@ -19,8 +45,25 @@ function GeneralInformation() {
   );
 }
 
-function EducationalExperience() {
-  return <></>;
+function EducationalExperience({ index }) {
+  return (
+    <>
+      <h2>Education {index}</h2>
+      <InputField
+        type="text"
+        userGuidance={"Education Facility Name"}
+      ></InputField>
+      <InputField type="text" userGuidance={"Title of Subject"}></InputField>
+      <InputField type="date" userGuidance={"Date started"}></InputField>
+      <InputField type="date" userGuidance={"Date finished"}></InputField>
+      <FreeTypingArea
+        type="text"
+        userGuidance={"Education description"}
+        rows={5}
+        cols={33}
+      ></FreeTypingArea>
+    </>
+  );
 }
 
 function PracticalExperience() {
@@ -31,12 +74,23 @@ function MoreInformation() {
   return <></>;
 }
 
-function InputField({ type, userGuidance }) {
+function InputField({ type = "text", userGuidance }) {
   return (
     <label>
       {" "}
       {userGuidance}
       <input type={type}></input>
     </label>
+  );
+}
+
+function FreeTypingArea({ id, rows, cols, userGuidance }) {
+  return (
+    <textarea
+      id={id}
+      rows={rows}
+      cols={cols}
+      placeholder={userGuidance}
+    ></textarea>
   );
 }
