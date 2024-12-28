8b67b4389e79dedc35f909db5deeb63292760d0b
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2024-12-28 14:52:24 +0400

    Initial structure created

---
+++ src/components/edit-bar.jsx
@@ -0,0 +1,26 @@
+export default function EditBar() {
+  return (
+    <>
+      <GeneralInformation></GeneralInformation>
+      <EducationalExperience></EducationalExperience>
+      <PracticalExperience></PracticalExperience>
+      <MoreInformation></MoreInformation>
+    </>
+  );
+}
+
+function GeneralInformation() {
+  return <></>;
+}
+
+function EducationalExperience() {
+  return <></>;
+}
+
+function PracticalExperience() {
+  return <></>;
+}
+
+function MoreInformation() {
+  return <></>;
+}
